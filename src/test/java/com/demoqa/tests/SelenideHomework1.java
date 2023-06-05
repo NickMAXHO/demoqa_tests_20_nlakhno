e288b3a32b87fd853f2444044d81993163cacac0
Nikolay_Lakhno <nick.lakhno@gmail.com>
2023-06-05 11:50:48 +0600

    SelenideHomework1 fix

--- src/test/java/com/demoqa/tests/SelenideHomework1.java
+++ src/test/java/com/demoqa/tests/SelenideHomework1.java
@@ -1,10 +1,9 @@
 package com.demoqa.tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -21,9 +20,23 @@ public class SelenideHomework1 {
         $("#wiki-tab").click();
         // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").sendKeys("sof");
-        $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(Condition.text("SoftAssertions"));
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("SoftAssertions"));
         // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
-        $("#user-content-3-using-junit5-extend-test-class").$(byText(""));
+        $("#wiki-content .markdown-body")
+                .$$("div pre")
+                .findBy(text("@ExtendWith({SoftAssertsExtension.class})"))
+                .shouldHave(text("""
+                        class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                                               
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                        }"""));
+
     }
 }
